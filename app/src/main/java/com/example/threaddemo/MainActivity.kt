9c68358fc2287a3120d961b99c4d415fed56c38b
Toni Westerlund <toni.westerlund@machilli.com>
2021-09-24 21:58:17 +0300

    Add Thread Communication Example

--- app/src/main/java/com/example/threaddemo/MainActivity.kt
+++ app/src/main/java/com/example/threaddemo/MainActivity.kt
@@ -2,16 +2,32 @@ package com.example.threaddemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.util.Log
+import com.example.threaddemo.databinding.ActivityMainBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlin.concurrent.thread
 import kotlin.system.measureTimeMillis
 
+const val HELLO_MSG = 1
+const val PING_PONG_MSG = 2
+const val QUIT_MSG = 3
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        var mainThreadHandler : Handler? = null
 
         // Counter
         var counter = 0
@@ -58,5 +74,111 @@ class MainActivity : AppCompatActivity() {
         }
 
         Log.v("ToniWesterlund", "Do  $forLoopCount iteration in $time ms")
+
+
+        // Create and start Thread
+        val mySimpleThread = SimpleThread()
+        mySimpleThread.start()
+
+        // Create and start runnable (Thread)
+        val mySimpleRunnable = SimpleRunnable()
+        val myThread = Thread(mySimpleRunnable)
+        myThread.start()
+
+        mainThreadHandler = object : Handler(Looper.getMainLooper()){
+
+            override fun handleMessage(msg: Message) {
+                if(HELLO_MSG == msg.what){
+                    Log.v("ToniWesterlund" , "Hello World ${Thread.currentThread()}")
+                }
+            }
+        }
+
+        val myWorkerRunnable = WorkerRunnable(mainThreadHandler)
+        val myWorkerThread = Thread(myWorkerRunnable)
+
+
+        // Start Thread
+        binding.startButton.setOnClickListener{
+            myWorkerThread.start()
+        }
+        // Send Hello Message
+        binding.helloButton.setOnClickListener{
+            val msg = Message()
+            msg.what = HELLO_MSG
+            msg.obj = "Hello"
+            myWorkerRunnable.workerRunnableHandler!!.sendMessage(msg)
+
+        }
+        // Send Ping Pong Message
+        binding.pingButton.setOnClickListener{
+            val msg = Message()
+            msg.what = PING_PONG_MSG
+            msg.obj = "PING PONG"
+            myWorkerRunnable.workerRunnableHandler!!.sendMessage(msg)
+        }
+        // Send Quit Message
+        binding.quitButton.setOnClickListener{
+            val msg = Message()
+            msg.what = QUIT_MSG
+            msg.obj = "Quit"
+            myWorkerRunnable.workerRunnableHandler!!.sendMessage(msg)
+        }
     }
+}
+
+class WorkerRunnable(val mainThreadHandler : Handler?) : Runnable{
+
+    var workerRunnableHandler : Handler? = null
+
+    override fun run() {
+        Log.v("ToniWesterlund", "Run Start")
+
+        // Loop start Point
+        Looper.prepare()
+
+        workerRunnableHandler = object  : Handler(Looper.myLooper()!!){
+            override fun handleMessage(msg: Message) {
+
+                // Handle Messages
+                if(HELLO_MSG == msg.what){
+                    Log.v("ToniWesterlund", "Hello World ${Thread.currentThread()} ")
+                }else if(PING_PONG_MSG == msg.what){
+                    Log.v("ToniWesterlund", "PING")
+                    val msgReply = Message()
+                    msgReply.what = HELLO_MSG
+                    msgReply.obj = "Hello"
+
+                    // Reply Message back to main thread
+                    mainThreadHandler!!.sendMessage(msgReply)
+
+                }else if(QUIT_MSG == msg.what){
+                    Log.v("ToniWesterlund", "QUIT_MSG")
+                    Looper.myLooper()?.quit()
+                }
+
+            }
+        }
+        // Loop End Point
+        Looper.loop()
+
+    }
+
+}
+
+
+// SimpleThread Class, Inherited from Thread class
+class SimpleThread : Thread(){
+
+    public override fun run() {
+        Log.v("ToniWesterlund", "Thread Run - ${Thread.currentThread()} has run")
+    }
+}
+
+// SimpleRunnable, Implements Runnable interface
+class SimpleRunnable : Runnable{
+    override fun run() {
+        Log.v("ToniWesterlund", "Runnable Run - ${Thread.currentThread()} has run")
+    }
+
 }
